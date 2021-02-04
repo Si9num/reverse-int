93784b8a1477fb10e6865e54baaa600b2bd8ab23
Si9num <78493168+Si9num@users.noreply.github.com>
2021-02-05 02:17:14 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = n.toString().split("").reverse();
+    if (num.length > 3) {
+        let f = num.pop();
+    }
+    return +num.join("");
+
+    
 }
